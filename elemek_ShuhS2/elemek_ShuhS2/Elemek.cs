c3cbfb140d0d001d9ef5fb9ce39a031614554628
SchuhSoma <60043511+SchuhSoma@users.noreply.github.com>
2020-02-19 21:41:35 +0100

    Add files via upload

---
+++ elemek_ShuhS2/elemek_ShuhS2/Elemek.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace elemek_ShuhS2
+{
+    class Elemek
+    {
+        public int Rendszam;
+        public string Vegyjel;
+        public string Nev;
+        public double Atomtomeg;
+        public double Olvadasho;
+        public double Forrasho;
+        public double Elektronnegativ;
+
+        public Elemek (string sor)
+        {
+            var dbok = sor.Split(';');
+            this.Rendszam = int.Parse(dbok[0]);
+            this.Vegyjel = dbok[1];
+            this.Nev = dbok[2];
+            this.Atomtomeg = double.Parse(dbok[3]);
+            this.Olvadasho = double.Parse(dbok[4]);
+            this.Forrasho = double.Parse(dbok[5]);
+            this.Elektronnegativ = double.Parse(dbok[6]);
+        }
+    }
+}
